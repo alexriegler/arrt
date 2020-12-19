0d334d88f321b3827709f47bd932d9a468c1d18c
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-19 13:26:55 -0600

    Add a progress indicator

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -11,6 +11,7 @@ int main()
 
 	for (int j = image_height - 1; j >= 0; --j)
 	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
 			auto r = double(i) / (image_width - 1);
@@ -24,4 +25,6 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::cerr << "\nDone.\n";
 }

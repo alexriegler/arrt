7f7005c5d3b2c7ed3ff1b508a2e432c7cf5c7051
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-20 00:26:07 -0600

    Add front-face tracking

--- RayTracing/src/hittable.h
+++ RayTracing/src/hittable.h
@@ -7,6 +7,13 @@ struct hit_record {
 	point3 p;
 	vec3 normal;
 	double t;
+	bool front_face;
+
+	// TODO: Normals can either always point outward from surface or always point against the incident ray.
+	inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+		front_face = dot(r.direction(), outward_normal) < 0;
+		normal = front_face ? outward_normal : -outward_normal;
+	}
 };
 
 // TODO: Change name of hittable to something else.

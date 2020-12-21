f1b1530af9f1ffeafc53e6813a00e4b514a30093
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-21 17:36:20 -0600

    Add field of view

--- RayTracing/src/camera.h
+++ RayTracing/src/camera.h
@@ -2,14 +2,19 @@
 
 #include "utility.h"
 
-// TODO: Add stratification for antialiasing.
+// TODO: Look into stratification for antialiasing.
 class camera {
 public:
 	// Constructors
-	camera() {
-		const auto aspect_ratio = 16.0 / 9.0;
-		auto viewport_height = 2.0;
+	camera(
+		double vfov,	// vertical field-of-view in degrees
+		double aspect_ratio
+	) {
+		auto theta = degrees_to_radians(vfov);
+		auto h = tan(theta / 2);
+		auto viewport_height = 2.0 * h;
 		auto viewport_width = aspect_ratio * viewport_height;
+		
 		auto focal_length = 1.0;
 
 		origin = point3(0, 0, 0);

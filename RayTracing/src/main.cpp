e823282fb8052990dd1af6b91cfa72baa077ebbe
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-20 21:31:57 -0600

    Update main

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -1,5 +1,6 @@
 #include "utility.h"
 
+#include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
@@ -21,6 +22,7 @@ int main() {
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// World
 	hittable_list world;
@@ -28,17 +30,23 @@ int main() {
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
+	// Camera
+	camera cam;
+
 	//Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (double(image_width) - 1);
-			auto v = double(j) / (double(image_height) - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = (i + random_double()) / (double(image_width) - 1);
+				auto v = (j + random_double()) / (double(image_height) - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 

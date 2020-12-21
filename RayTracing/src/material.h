08e45554c8fd5d50b7bc1c48143d35f1a0ad570e
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-20 23:21:54 -0600

    Add lambertian material class

--- RayTracing/src/material.h
+++ RayTracing/src/material.h
@@ -2,11 +2,35 @@
 
 #include "utility.h"
 
-struct hit_record;
+#include "hittable.h"
 
 class material {
 public:
 	virtual bool scatter(
 		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
 	) const = 0;
+};
+
+class lambertian : public material {
+public:
+	// Constructor
+	lambertian(const color& a) : albedo(a) {}
+
+	// Member functions
+	// TODO: You can also scatter with some probability p and have attenuation be albedo/p.
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+		auto scatter_direction = rec.normal + random_unit_vector();
+
+		// Catch degenerate scatter direction
+		if (scatter_direction.near_zero()) {
+			scatter_direction = rec.normal;
+		}
+
+		scattered = ray(rec.p, scatter_direction);
+		attenuation = albedo;
+		return true;
+	}
+
+public:
+	color albedo;
 };

a4ed54ecefd07665ecf8d5ceff8ec45ccadfaab8
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-20 22:30:14 -0600

    Add true lambertian reflection

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -10,12 +10,13 @@
 color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
 
+	// If we've exceeded the ray bounce limit, no more light is gathered.
 	if (depth <= 0) {
 		return color(0, 0, 0);
 	}
 
 	if (world.hit(r, 0.001, infinity, rec)) {
-		point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		point3 target = rec.p + rec.normal + random_unit_vector();
 		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
 	}
 
--- RayTracing/src/vec3.h
+++ RayTracing/src/vec3.h
@@ -122,3 +122,7 @@ inline vec3 random_in_unit_sphere() {
 		return p;
 	}
 }
+
+inline vec3 random_unit_vector() {
+	return unit_vector(random_in_unit_sphere());
+}

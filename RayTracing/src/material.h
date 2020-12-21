9926487c4910d64648a4dfcc4496f2d580b3af67
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-21 17:24:17 -0600

    Demo hollow glass sphere

--- RayTracing/src/material.h
+++ RayTracing/src/material.h
@@ -95,6 +95,6 @@ private:
 		// Use Shlick's approximation for reflectance.
 		auto r0 = (1 - ref_idx) / (1 + ref_idx);
 		r0 = r0 * r0;
-		return r0 + (1 - r0) * pow((1 - cosine), 5);
+		return r0 + (1 - r0) * pow((1 - cosine), 5); 
 	}
 };

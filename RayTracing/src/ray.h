ae273aa7c789483fffbfb304555d40a2c6829791
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-22 15:54:37 -0600

    Add time to ray info

--- RayTracing/src/ray.h
+++ RayTracing/src/ray.h
@@ -6,13 +6,14 @@ class ray {
 public:
 	// Constructors
 	ray() {}
-	ray(const point3& origin, const vec3& direction)
-		: orig(origin), dir(direction)
+	ray(const point3& origin, const vec3& direction, double time = 0.0)
+		: orig(origin), dir(direction), tm(time)
 	{}
 
 	// Member functions
 	point3 origin() const { return orig; }
 	vec3 direction() const { return dir; }
+	double time() const { return tm; }
 
 	point3 at(double t) const {
 		return orig + t * dir;
@@ -22,4 +23,5 @@ public:
 public:
 	point3 orig;
 	vec3 dir;
+	double tm;
 };

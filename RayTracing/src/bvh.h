dda0a1df70eef7fbed1aadffa13ae96c12875462
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-23 19:57:39 -0600

    Add bvh split algorithm

--- RayTracing/src/bvh.h
+++ RayTracing/src/bvh.h
@@ -5,6 +5,8 @@
 #include "hittable.h"
 #include "hittable_list.h"
 
+#include <algorithm>
+
 class bvh_node : public hittable {
 public:
 	// Constructors
@@ -27,6 +29,55 @@ public:
 	shared_ptr<hittable> right;
 	aabb box;
 };
+
+bvh_node::bvh_node(
+	const std::vector<shared_ptr<hittable>>& src_objects,
+	size_t start, size_t end, double time0, double time1) {
+	auto objects = src_objects;		// Create a modifiable array of the source scene objects.
+
+	int axis = random_int(0, 2);
+	auto comparator = (axis == 0) ? box_x_compare
+		            : (axis == 1) ? box_y_compare
+		                          : box_z_compare;
+
+	size_t object_span = end - start;
+
+	switch (object_span)
+	{
+	case 1:
+		left = right = objects[start];
+		break;
+	case 2:
+		if (comparator(objects[start], objects[start + 1])) {
+			left = objects[start];
+			right = objects[start + 1];
+		}
+		else {
+			left = objects[start + 1];
+			right = objects[start];
+		}
+		break;
+	//case 3:
+		// TODO: Check explicitly for 3 elements. 1 recursion and 1 leaf.
+	default:
+		// TODO: Use std::parallel algorithm.
+		std::sort(objects.begin() + start, objects.begin() + end, comparator);
+
+		auto mid = start + object_span / 2;
+		left = make_shared<bvh_node>(objects, start, mid, time0, time1);
+		right = make_shared<bvh_node>(objects, mid, end, time0, time1);
+		break;
+	}
+
+	aabb box_left, box_right;
+
+	if (!(left->bounding_box(time0, time1, box_left)) || !(right->bounding_box(time0, time1, box_right))) {
+		// TODO: Maybe throw exception here.
+		std::cerr << "No bounding box in bvh_node constructor.\n";
+	}
+
+	box = surrounding_box(box_left, box_right);
+}
 
 bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
 	if (!box.hit(r, t_min, t_max)) {
--- RayTracing/src/utility.h
+++ RayTracing/src/utility.h
@@ -20,7 +20,7 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-	// Returns a random real in [0,1).
+	// Returns a random real in [0, 1).
 	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
 	static std::mt19937 generator;
 	return distribution(generator);
@@ -29,6 +29,11 @@ inline double random_double() {
 inline double random_double(double min, double max) {
 	// Returns a random real in [min, max).
 	return min + (max - min) * random_double();
+}
+
+inline int random_int(int min, int max) {
+	// Returns a random integer in [min, max].
+	return static_cast<int>(random_double(min, max + 1));
 }
 
 inline double clamp(double x, double min, double max) {

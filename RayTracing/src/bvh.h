56257db16ba0bf5665ebec9ecb71c1dfabf5713c
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-23 20:09:41 -0600

    Add box comparison functions

--- RayTracing/src/bvh.h
+++ RayTracing/src/bvh.h
@@ -30,6 +30,30 @@ public:
 	aabb box;
 };
 
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+	aabb box_a;
+	aabb box_b;
+
+	if (!(a->bounding_box(0, 0, box_a)) || !(b->bounding_box(0, 0, box_b))) {
+		// TODO: Maybe throw exception here.
+		std::cerr << "No bounding box in bvh_node constructor.\n";
+	}
+
+	return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+	return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+	return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+	return box_compare(a, b, 2);
+}
+
 bvh_node::bvh_node(
 	const std::vector<shared_ptr<hittable>>& src_objects,
 	size_t start, size_t end, double time0, double time1) {
@@ -61,6 +85,7 @@ bvh_node::bvh_node(
 		// TODO: Check explicitly for 3 elements. 1 recursion and 1 leaf.
 	default:
 		// TODO: Use std::parallel algorithm.
+		// TODO: Use lambda instead of 'comparator' variable.
 		std::sort(objects.begin() + start, objects.begin() + end, comparator);
 
 		auto mid = start + object_span / 2;

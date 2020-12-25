f5b0d2120d1eded2020679c8dca40101feccad4c
Alex Riegler <alexander.s.riegler@gmail.com>
2020-12-24 22:58:35 -0600

    Comment out marble texture

--- RayTracing/src/texture.h
+++ RayTracing/src/texture.h
@@ -64,8 +64,10 @@ public:
 	// Member function
 	virtual color value(double u, double v, const point3& p) const override {
 		// TODO: Rename perlin noise so we don't have noise.noise.
-		// return color(1, 1, 1) * noise.turb(scale * p);
-		return color(1, 1, 1) * 0.5 * (1 + sin(scale * p.z() + 10 * noise.turb(p)));
+		return color(1, 1, 1) * noise.turb(scale * p);
+		
+		// Marbled pattern:
+		// return color(1, 1, 1) * 0.5 * (1 + sin(scale * p.z() + 10 * noise.turb(p)));
 	}
 
 public:

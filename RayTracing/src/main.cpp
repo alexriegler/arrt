fa03fdb8e696201440f0c4a4dbf50374b95591c8
Alex Riegler <alexander.s.riegler@gmail.com>
2021-01-05 17:40:50 -0600

    Add box class

--- RayTracing/src/main.cpp
+++ RayTracing/src/main.cpp
@@ -1,6 +1,7 @@
 #include "utility.h"
 
 #include "aarect.h"
+#include "box.h"
 #include "bvh.h"
 #include "camera.h"
 #include "color.h"
@@ -152,6 +153,9 @@ hittable_list cornell_box() {
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
 	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+	objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+	objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
 	
 	return objects;
 }

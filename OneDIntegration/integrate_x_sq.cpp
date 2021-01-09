b63b1ff3160fa6b555c783fa5c45142c5c29cd8e
Alex Riegler <alexander.s.riegler@gmail.com>
2021-01-09 12:51:06 -0600

    Add pdf

--- OneDIntegration/integrate_x_sq.cpp
+++ OneDIntegration/integrate_x_sq.cpp
@@ -5,14 +5,18 @@
 #include <math.h>
 #include <stdlib.h>
 
+inline double pdf(double x) {
+	return 3 * x * x / 8;
+}
+
 int main() {
-	int N = 1000000;
+	int N = 1;
 	auto sum = 0.0;
 	for (int i = 0; i < N; ++i) {
-		auto x = random_double(0, 2);
-		sum += x * x;
+		auto x = pow(random_double(0, 8), 1.0/3.0);
+		sum += x * x / pdf(x);
 	}
-	auto integral = 2 * sum / N;
+	auto integral = sum / N;
 	std::cout << std::fixed << std::setprecision(12);
 	std::cout << "I = " << integral << '\n';
 }

9751ca9918c71d4ee79d3b88e9c4bc3b5338e005
Alex Riegler <alexander.s.riegler@gmail.com>
2021-01-09 11:51:39 -0600

    Add infinite pi estimate

--- MonteCarlo/pi.cpp
+++ MonteCarlo/pi.cpp
@@ -6,16 +6,20 @@
 #include <stdlib.h>
 
 int main() {
-	int N = 1000;
 	int inside_circle = 0;
-	for (int i = 0; i < N; ++i) {
+	int runs = 0;
+	std::cout << std::fixed << std::setprecision(12);
+	while (true) {
+		runs++;
 		auto x = random_double(-1, 1);
 		auto y = random_double(-1, 1);
 		if ((x * x) + (y * y) < 1) {
 			inside_circle++;
 		}
+
+		if (runs % 100000 == 0) {
+			auto pi_estimate = 4 * static_cast<double>(inside_circle) / runs;
+			std::cout << "Estimate of Pi = " << pi_estimate << '\r';
+		}
 	}
-	auto pi_estimate = 4 * static_cast<double>(inside_circle) / N;
-	std::cout << std::fixed << std::setprecision(12);
-	std::cout << "Estimate of Pi = " << pi_estimate << '\n';
 }
